b0e4a1b88537dc16ae6a18a68d3ad12bfa5ff423
Eric Jones <ericdiemerj@gmail.com>
2022-05-25 08:51:03 -0400

    ew ul and runningwith a more updated go version.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TheDiemer/fun_with_go
+
+go 1.18
--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	fmt.Println("testtest", "test")
-	resp, err := http.Get("https://google.com")
+	// resp, err := http.Get("https://google.com")
+	resp, err := http.Get("https://en.wikipedia.org/api/rest_v1/page/random/summary")
 	defer resp.Body.Close()
 	if err != nil {
 		fmt.Println("experienced error:",err)
